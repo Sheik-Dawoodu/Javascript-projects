f70a9be8a12f508dae8933a1a7792c872354766d
Sheik-Dawoodu <142398453+Sheik-Dawoodu@users.noreply.github.com>
2023-09-11 13:43:43 +0530

    initial commit

---
+++ Events/Onclick-Event/onclick.js
@@ -0,0 +1,10 @@
+let on=() =>{
+    document.getElementById("btnon").src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png'
+    document.getElementById("btnoff").src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png'
+}
+
+let off=()=>{
+    document.getElementById("btnon").src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png'
+    document.getElementById("btnoff").src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png'
+
+}
